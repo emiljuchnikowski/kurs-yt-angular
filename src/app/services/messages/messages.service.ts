e28a9523c103b8f5781613ca64c0a5425fdce886
Emil Juchnikowski <emil@juchnikowski.com>
2023-08-18 11:15:42 +0200

    Klient HTTP

--- src/app/services/messages/messages.service.ts
+++ src/app/services/messages/messages.service.ts
@@ -1,22 +1,28 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, lastValueFrom } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class MessagesService {
-    private _source = new BehaviorSubject<string[]>([
-        "Wiadomosc 1",
-        "Wiadomosc 2",
-        "Wiadomosc 3",
-      ]);
+    private _source = new BehaviorSubject<string[]>([]);
 
     list$ = this._source.asObservable();
+
+    constructor(private http: HttpClient) {
+        this.init();
+    }
 
     add(val: string): void {
         this._source.next([
             ...this._source.value,
             val
         ]);
+    }
+
+    private async init(): Promise<void> {
+        const data = await lastValueFrom(this.http.get<string[]>("/assets/messages.json"));
+        this._source.next(data);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app-routing';
+import { AuthInterceptor } from './app/interceptors';
 
 
 bootstrapApplication(AppComponent, {
     providers: [
+      importProvidersFrom(HttpClientModule),
       importProvidersFrom(BrowserModule),
-      provideRouter(routes)
+      provideRouter(routes),
+      {
+        provide: HTTP_INTERCEPTORS,
+        multi: true,
+        useClass: AuthInterceptor
+      }
     ]
 })
   .catch(err => console.error(err));
